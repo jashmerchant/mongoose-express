0962b51a3db5619c4a17ca625af63af5930d7025
unknown <merchantjash@gmail.com>
2021-05-07 17:44:44 +0530

    Implement CRUD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const colors = require('colors');
 const path = require('path');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override');
 const Product = require('./models/product');
 
 //***Connect Mongoose with Mongodb*** 
@@ -28,10 +28,51 @@ app.set('views', path.join(__dirname, '/views'));
 //***Static Files***
 app.use(express.static(path.join(__dirname, '/public')));
 
+//***Method-Override for PUT req*** 
+app.use(methodOverride('_method'))
+
 //***Routes***
+app.get('/', (req, res) => {
+    res.redirect('/products');
+})
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
 })
 
 //***Listener****
