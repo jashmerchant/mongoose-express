23d64118d5a9ccb2395221c6a2d0b81801624a4e
unknown <merchantjash@gmail.com>
2021-05-18 11:54:34 +0530

    Add finishing touches

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/farms/new', (req, res) => {
 })
 
 app.get('/farms/:id', async (req, res) => {
-    const farm = await Farm.findById(req.params.id);
+    const farm = await Farm.findById(req.params.id).populate("products");
     res.render('farms/show', { farm });
 })
 
@@ -66,12 +66,12 @@ app.post('/farms/:id/products', async (req, res) => {
     const { id } = req.params;
     const farm = await Farm.findById(id);
     const { name, price, category } = req.body;
-    const product = await new Product({ name, price, category });
-    farm.product.push(products);
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
     product.farm = farm;
     await farm.save();
     await product.save();
-    res.send(farm);
+    res.redirect(`/farms/${id}`);
 })
 
 
